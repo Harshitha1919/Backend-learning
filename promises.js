c60b2f9009ad479f26358cd3f706198be06c527a
Harshitha1919 <saiharshithamuchhu@gmail.com>
2025-12-04 11:16:18 +0530

    commit

--- promises.js
+++ promises.js
@@ -1,20 +1,42 @@
-function getUserDetails(id){
-
-}
+function getUserEmail(email){
+    return new Promise((resolve,reject)=>{
+        console.log("User email is ",email);
+        resolve({name:"sarayu"})
+    })
+    
 };
-function getUserSubjects(suerRoll){
-    return new Promise((resolve,reject)=>){
-        console.log("fun2 called")
-        resolve((userSubId:'en-1'));
+function getUserName(name){
+    return new Promise((resolve,reject)=>{
+     console.log("User Name is ",name);
+      resolve({status:"success"})
     })
 }
-function getUsermarks(userSubId){
-    return new Promise((resolve,reject()=>){
-        console.log("fun3 called")
-        resolve("getting user marks with subid",userSubjectId    
-        })
-    };
-    getUserDetails("123").then(result)=>{
-         return getUserSubjects(result.rollnumber)
-    });
+function showLoginStatus(status){
+    return new Promise((resolve,reject)=>{
+       console.log("Login Status is",status);
+       resolve({status:"success"});
+    })
 }
+
+getUserEmail("allampallisarayu64@gmail.com").then((result)=>{
+    return getUserName(result.name)
+}).then((result)=>{
+    return showLoginStatus(result.status)
+}).then((result)=>console.log(result)).catch((error)=>{
+    console.log(error);
+});
+
+// const promise = new Promise((resolve, reject) => {
+//     if(true){
+//         resolve("success");
+//     }
+//     else{
+//         reject("failure");
+//     }
+// })
+// promise.then((result) => {
+//     console.log("success");
+// })
+// .catch((error) => {
+//     console.log("failure");
+// });
